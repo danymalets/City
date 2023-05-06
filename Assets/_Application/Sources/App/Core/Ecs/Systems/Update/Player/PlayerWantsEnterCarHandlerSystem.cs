ee28da812ab7616201386b7c23c56762116e7bfc
danymalets <malets.d.l@gmail.com>
2023-05-07 02:50:30 +0300

    enter nav system done; fix check box collider

--- Assets/_Application/Sources/App/Core/Ecs/Systems/Update/Player/PlayerWantsEnterCarHandlerSystem.cs
+++ Assets/_Application/Sources/App/Core/Ecs/Systems/Update/Player/PlayerWantsEnterCarHandlerSystem.cs
@@ -1,9 +1,15 @@
 using Scellecs.Morpeh;
+using Sources.App.Core.Ecs.Aspects;
+using Sources.App.Core.Ecs.Components.NavPathes;
 using Sources.App.Core.Ecs.Components.Player;
 using Sources.App.Core.Ecs.Components.Tags;
 using Sources.App.Data.Cars;
+using Sources.App.Data.Common;
+using Sources.Utils.Di;
+using Sources.Utils.MorpehWrapper.DefaultComponents.Views;
 using Sources.Utils.MorpehWrapper.MorpehUtils.Extensions;
 using Sources.Utils.MorpehWrapper.MorpehUtils.Systems;
+using UnityEngine;
 
 namespace Sources.App.Core.Ecs.Systems.Update.Player
 {
@@ -11,6 +17,12 @@ namespace Sources.App.Core.Ecs.Systems.Update.Player
     {
         private Filter _filter;
         private Filter _carsFilter;
+        private readonly INavigationService _navigationService;
+
+        public PlayerWantsEnterCarHandlerSystem()
+        {
+            _navigationService = DiContainer.Resolve<INavigationService>();
+        }
 
         protected override void OnInitFilters()
         {
@@ -21,8 +33,34 @@ namespace Sources.App.Core.Ecs.Systems.Update.Player
         {
             foreach (Entity playerEntity in _filter)
             {
+                Debug.Log($"player wants");
+
                 CarPlaceData carPlaceData = playerEntity.Get<PlayerWantsEnterCarEvent>().CarPlaceData;
-                playerEntity.Set(new PlayerEnterCarEvent { CarPlaceData = carPlaceData });
+
+                IEnterPoint enterPoint = carPlaceData.Car.GetAspect<CarPassengersAspect>()
+                    .GetPlaceEnterPoint(carPlaceData.Place);
+
+                if (_navigationService.TryGetPlayerPath(
+                        playerEntity.GetRef<IRigidbody>().Position,
+                        enterPoint.Position, out Vector3[] path))
+                {
+                    playerEntity.Set(new OnNavPath
+                    {
+                        Path = path,
+                        LastCompetedPoint = 0,
+                    });
+
+                    playerEntity.Set(new OnNavToCar
+                    {
+                        PlaceData = carPlaceData
+                    });
+                    // Debug.Log($"path start:{Vector3.Distance(playerEntity.GetRef<IRigidbody>().Position, path[0])} " +
+                    //           $"end:{Vector3.Distance(path[^1], enterPoint.Position)}");
+                }
+                else
+                {
+                    playerEntity.Add<OnNavToCarFailedEvent>();
+                }
             }
         }
     }
